d857a2590717950cfbb83a9ceb8cd22b2d6ebe8a
CodeMangler <hsdpal@gmail.com>
2013-02-11 21:38:17 +0530

    Switching to WMI APIs for setting display brightness..

--- Radiance/Radiance.fs
+++ Radiance/Radiance.fs
@@ -12,10 +12,22 @@ type Radiance() as this =
 
     member this.Window = window
 
+//    member this.Initialize = 
+//        let slider = window.FindName("brightness") :?> Slider
+//        let display = Monitor(window)
+//        do display.Initialize
+//        do slider.Minimum <- float(display.MinimumBrightness)
+//        do slider.Maximum <- float(display.MaximumBrightness)
+//        do slider.Value <- float(display.CurrentBrightness)
+
     member this.Initialize = 
         let slider = window.FindName("brightness") :?> Slider
-        let display = Monitor(window)
-        do display.Initialize
-        do slider.Minimum <- float(display.MinimumBrightness)
-        do slider.Maximum <- float(display.MaximumBrightness)
-        do slider.Value <- float(display.CurrentBrightness)
+        slider.ValueChanged.Add(fun _ -> this.SyncBrightness)
+        do slider.Minimum <- 0.0
+        do slider.Maximum <- 100.0
+        do slider.Value <- 50.0
+
+    member this.SyncBrightness = 
+        let slider = window.FindName("brightness") :?> Slider
+        let display = WMIMonitor()
+        do display.SetBrightness(slider.Value)
---
+++ Radiance/WMI.fs
@@ -0,0 +1,28 @@
+﻿namespace Radiance
+
+open System
+open System.Management
+
+type WMIMonitor() as this =
+    let setBrightness = new ManagementClass("WmiMonitorBrightnessMethods")
+    do setBrightness.Scope <- new ManagementScope(@"\\.\root\wmi")
+    let monitorInstances = setBrightness.GetInstances()
+
+    interface IDisposable with
+        member this.Dispose() = 
+            for monitorInstance in monitorInstances do
+                monitorInstance.Dispose()
+            done
+            do setBrightness.Dispose()
+    
+    member this.SetBrightness(percent) =
+            for monitorInstance in monitorInstances do
+                let active = monitorInstance.GetPropertyValue("Active")
+                match (active :?> bool) with
+                    | true ->
+                        let timeout = 1 //seconds
+                        let brightness = percent
+                        (monitorInstance :?> ManagementObject).InvokeMethod("WmiSetBrightness", [|timeout; brightness|]) |> ignore
+                    | _ -> 
+                        Console.Out.WriteLine("Skipping inactive monitor") |> ignore
+            done
