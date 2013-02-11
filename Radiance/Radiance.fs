c91ccaf9229e1ef5ce860ee358956ae7223e8ab3
CodeMangler <hsdpal@gmail.com>
2013-02-12 01:31:04 +0530

    Brightness control PoC (ugly code) using WMI
    Tested on Windows 7 and Windows 8

--- Radiance/Radiance.fs
+++ Radiance/Radiance.fs
@@ -9,25 +9,19 @@ open Radiance.WPF
 type Radiance() as this =
     let window = createWindow("Radiance.xaml")
     do window.Loaded.AddHandler(fun obj args -> this.Initialize |> ignore)
+    let monitor = new WMIMonitor()
+    do monitor.Initialize()
 
     member this.Window = window
 
-//    member this.Initialize = 
-//        let slider = window.FindName("brightness") :?> Slider
-//        let display = Monitor(window)
-//        do display.Initialize
-//        do slider.Minimum <- float(display.MinimumBrightness)
-//        do slider.Maximum <- float(display.MaximumBrightness)
-//        do slider.Value <- float(display.CurrentBrightness)
-
     member this.Initialize = 
         let slider = window.FindName("brightness") :?> Slider
-        slider.ValueChanged.Add(fun _ -> this.SyncBrightness)
-        do slider.Minimum <- 0.0
-        do slider.Maximum <- 100.0
-        do slider.Value <- 50.0
+        slider.ValueChanged.Add(fun _ -> this.SyncBrightness())
+        do slider.Minimum <- float(monitor.MinimumBrightness)
+        do slider.Maximum <- float(monitor.MaximumBrightness)
+        do slider.Value <- float(monitor.CurrentBrightness)
 
-    member this.SyncBrightness = 
+    member this.SyncBrightness() = 
         let slider = window.FindName("brightness") :?> Slider
-        let display = WMIMonitor()
+        let display = new WMIMonitor()
         do display.SetBrightness(slider.Value)
--- Radiance/WMI.fs
+++ Radiance/WMI.fs
@@ -3,19 +3,39 @@
 open System
 open System.Management
 
-type WMIMonitor() as this =
-    let setBrightness = new ManagementClass("WmiMonitorBrightnessMethods")
-    do setBrightness.Scope <- new ManagementScope(@"\\.\root\wmi")
-    let monitorInstances = setBrightness.GetInstances()
+type WMIMonitor() =
+    let brightness = new ManagementClass(@"\\.\root\wmi", "WmiMonitorBrightness", ObjectGetOptions())
+    let brightnessSetter = new ManagementClass(@"\\.\root\wmi", "WmiMonitorBrightnessMethods", ObjectGetOptions())
 
-    interface IDisposable with
-        member this.Dispose() = 
-            for monitorInstance in monitorInstances do
-                monitorInstance.Dispose()
-            done
-            do setBrightness.Dispose()
+    let mutable _minimumBrightness = 0ul
+    let mutable _currentBrightness = 0ul
+    let mutable _maximumBrightness = 0ul
+
+    member this.MinimumBrightness
+        with get() = _minimumBrightness
+        and set(value) = _minimumBrightness <- value
+
+    member this.CurrentBrightness
+        with get() = _currentBrightness
+        and set(value) = _currentBrightness <- value
+
+    member this.MaximumBrightness
+        with get() = _maximumBrightness
+        and set(value) = _maximumBrightness <- value
     
+    member this.GetBrightness() =
+            use monitorInstances = brightness.GetInstances()
+            for monitorInstance in monitorInstances do
+                let active = monitorInstance.GetPropertyValue("Active")
+                match (active :?> bool) with
+                    | true ->
+                        this.CurrentBrightness <- System.UInt32.Parse(monitorInstance.GetPropertyValue("CurrentBrightness").ToString())
+                    | _ ->
+                        this.CurrentBrightness <- uint32(0)
+            done
+
     member this.SetBrightness(percent) =
+            use monitorInstances = brightnessSetter.GetInstances()
             for monitorInstance in monitorInstances do
                 let active = monitorInstance.GetPropertyValue("Active")
                 match (active :?> bool) with
@@ -26,3 +46,13 @@ type WMIMonitor() as this =
                     | _ -> 
                         Console.Out.WriteLine("Skipping inactive monitor") |> ignore
             done
+
+    member this.Initialize() = 
+        do this.MinimumBrightness <- uint32(0)
+        do this.MaximumBrightness <- uint32(100)
+        do this.GetBrightness()
+
+    interface IDisposable with
+        member this.Dispose() = 
+            do brightness.Dispose()
+            do brightnessSetter.Dispose()
